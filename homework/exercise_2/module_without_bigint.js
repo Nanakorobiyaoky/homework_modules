b1fc27c9aef005494b570d6bdd0c01db27561d1b
Nanakorobiyaoky <dima.zhulin0@gmail.com>
2023-02-28 22:27:40 +0400

    Домашнее задание
    - переписал задание 2 без использования Bigint

---
+++ homework/exercise_2/module_without_bigint.js
@@ -0,0 +1,187 @@
+'use strict';
+
+module.exports = {
+    sum,
+    sub,
+    mul,
+    div,
+
+};
+
+function sum(a, b) {
+    let isNegative = false;
+
+    if (a.startsWith('-') && b.startsWith('-')) {
+        a = a.slice(1);
+        b = b.slice(1);
+        isNegative = true;
+    } else if (a.startsWith('-')) {
+        return sub(b, a.slice(1));
+    } else if (b.startsWith('-')) {
+        return sub(a, b.slice(1));
+    }
+
+    let firstOperand = a.split('').reverse();
+    let secondOperand = b.split('').reverse();
+
+    if (firstOperand.length < secondOperand.length) {
+        [firstOperand, secondOperand] = [secondOperand, firstOperand];
+    }
+
+    let resultArr = [];
+    let storedValue = 0
+
+    for (let i = 0; i < firstOperand.length; i++) {
+        let value = +firstOperand.at(i) + (+secondOperand.at(i) ? +secondOperand.at(i) : 0);
+        let currentValue = value + storedValue;
+        let pushValue = currentValue % 10;
+
+        resultArr.push(pushValue);
+        storedValue = (currentValue - pushValue) / 10;
+
+    }
+    resultArr.push(storedValue);
+    let result = resultArr.reverse().join('').replace(/^0+(0?.+)/, '$1');
+    if (result === '0') return result;
+    return (isNegative ? '-' : '') + result;
+
+}
+
+function sub(a, b) {
+    if (a.startsWith('-') && b.startsWith('-')) {
+        a = a.slice(1);
+        b = b.slice(1);
+        [a, b] = [b, a]
+    } else if (a.startsWith('-')) {
+        return sum(a, '-' + b);
+    } else if (b.startsWith('-')) {
+        return sum(a, b.slice(1));
+    }
+
+    let firstOperand = a.split('').reverse();
+    let secondOperand = b.split('').reverse();
+    let isNegative = false;
+
+    if (firstOperand.length < secondOperand.length) {
+        [firstOperand, secondOperand] = [secondOperand, firstOperand];
+        isNegative = true;
+
+    } else if (firstOperand.length === secondOperand.length) {
+        for (let i = -1; i > -firstOperand.length; i--) {
+            if (secondOperand.at(i) > firstOperand.at(i)) {
+                [firstOperand, secondOperand] = [secondOperand, firstOperand];
+                isNegative = true;
+                break;
+
+            } else if ( secondOperand.at(i) < firstOperand.at(i) ) {
+                break;
+            }
+        }
+    }
+
+    let resultArr = [];
+    let storedValue = 0
+
+    for (let i = 0; i < firstOperand.length; i++) {
+        let value = +firstOperand.at(i) + storedValue - (+secondOperand.at(i) ? +secondOperand.at(i) : 0);
+
+        if (value < 0) {
+            value = +firstOperand.at(i) + storedValue + 10 - (+secondOperand.at(i) ? +secondOperand.at(i) : 0);
+            storedValue = -1
+
+        } else {
+            storedValue = 0
+
+        }
+        resultArr.push(value);
+
+    }
+    let result = resultArr.reverse().join('').replace(/^0+(0?.+)/, '$1');
+    if (result === '0') return result;
+    return (isNegative ? '-' : '') + result;
+
+}
+
+function mul(a, b) {
+    let isNegative = false;
+
+    if (a.startsWith('-') && b.startsWith('-')) {
+        a = a.slice(1);
+        b = b.slice(1);
+    } else if (a.startsWith('-')) {
+        a = a.slice(1);
+        isNegative = true;
+    } else if (b.startsWith('-')) {
+        b = b.slice(1);
+        isNegative = true;
+    }
+
+    let firstOperand = a.split('').reverse();
+    let secondOperand = b.split('').reverse();
+
+    if (firstOperand.length < secondOperand.length) {
+        [firstOperand, secondOperand] = [secondOperand, firstOperand];
+    }
+
+    let terms = [];
+    
+    for (let j = 0; j < secondOperand.length; j++) {
+        let term = firstOperand.slice();
+        term = term.map(item => +item * +secondOperand.at(j));
+
+        let termArr = [];
+        let storedValue = 0
+
+        for (let i = 0; i < term.length; i++) {
+            let value = +term.at(i) + storedValue
+            termArr.push(value % 10)
+            storedValue = (value - (value % 10)) / 10
+        }
+        termArr.push(storedValue)
+        terms.push( termArr.reverse().join('') + ('0').repeat(j) )
+
+    }
+    let result = terms.reduce( (a, b) => sum(a, b), '0');
+    if (result === '0') return result
+    return (isNegative ? '-' : '') + result
+}
+
+function div(a, b) {
+    let isNegative = false;
+
+    if (a.startsWith('-') && b.startsWith('-')) {
+        a = a.slice(1);
+        b = b.slice(1);
+    } else if (a.startsWith('-')) {
+        a = a.slice(1);
+        isNegative = true;
+    } else if (b.startsWith('-')) {
+        b = b.slice(1);
+        isNegative = true;
+    }
+
+    let firstOperand = a;
+    let secondOperand = b;
+
+    let i = 0;
+    let result = '0';
+    while (true) {
+        let resultLen = result.length;
+        let firstOperandLen = firstOperand.length;
+        if (resultLen === firstOperandLen) {
+            for (let j = 0; j < resultLen; j++) {
+
+                if (result.at(j) > firstOperand.at(j)) return (isNegative ? '-' : '') + (i - 1).toString();
+                else if (result.at(j) === firstOperand.at(j)) continue
+                else break
+            }
+
+        } else if (resultLen > firstOperandLen) {
+            return (isNegative ? '-' : '') + (i - 1).toString()
+        }
+
+        i++
+        result = mul(secondOperand, i.toString())
+
+    }
+}
--- homework/exercise_2/test.js
+++ homework/exercise_2/test.js
@@ -1,33 +1,97 @@
 'use strict';
 
-const {sum, sub, mul, div} = require("./module.js");
+let {sub, sum, mul, div} = require("./module_without_bigint.js");
 
 
-// number, number
-console.log(sum('10123', '1432'));
-console.log(sub('45341', '1231'));
-console.log(mul('1785', '2222'));
-console.log(div('13', '23'));
-console.log();
+function checkSub() {
+    let i = 0;
+    let mistakeFlag = false;
+    while (true) {
+        let a = (Math.random() > 0.5 ? '-' : '') + Math.floor(Math.random() * 100_000_000_000).toString();
+        let b = (Math.random() > 0.5 ? '-' : '') + Math.floor(Math.random() * 100_000_000_000).toString();
 
-// bigint, bigint
+        let myOperation = sub(a.toString(), b.toString());
+        let jsOperation = (+a - +b).toString();
+        if (!(myOperation === jsOperation)) {
+            console.log('a:', a, 'b:', b);
+            console.log(myOperation, jsOperation);
+            mistakeFlag = true;
+        }
 
-console.log(sum('1000000000000000000000000000000000', '200000000000000000000000000000000000'));
-console.log(sub('16928356982345698325683756823945634', '16928356982345698325683756823945634'));
-console.log(mul('555555555555555555555555555555', '5555555555555555555555555555555555555'));
-console.log(div('99999999999999999999999999999', '3333333333333333333333333333333333'));
-console.log();
+        i++;
+        if (i === 10000) break;
+    }
+    mistakeFlag ? console.log('mistakes in sub') : console.log('sub is ok');
+}
 
-// number, bigint
+function checkSum() {
+    let i = 0;
+    let mistakeFlag = false;
+    while (true) {
+        let a = (Math.random() > 0.5 ? '-' : '') + Math.floor(Math.random() * 100_000_000_000).toString();
+        let b = (Math.random() > 0.5 ? '-' : '') + Math.floor(Math.random() * 100_000_000_000).toString();
 
-console.log(sum('9', '200000000000000000000000000000000000'));
-console.log(sub('0', '16928356982345698325683756823945634'));
-console.log(mul('2', '256488888888888888888888888888888884564'));
-console.log(div('3', '54635658468465656854642536546541684'));
-console.log(div('54635658468465656854642536546541684', '3'));
-console.log();
+        let myOperation = sum(a.toString(), b.toString());
+        let jsOperation = (+a + +b).toString();
+        if (!(myOperation === jsOperation)) {
+            console.log('a:', a, 'b:', b);
+            console.log(myOperation, jsOperation);
+            mistakeFlag = true;
+        }
 
-console.log(sum('sdfdsf', 'sdfsd'));
-console.log(sub('sdfdsf', 'sdfsd'));
-console.log(mul('sdfdsf', 'sdfsd'));
-console.log(div('sdfdsf', 'sdfsd'));
+        i++;
+        if (i === 10000) break;
+    }
+    mistakeFlag ? console.log('mistakes in sum') : console.log('sum is ok');
+}
+
+function checkMul() {
+    let i = 0;
+    let mistakeFlag = false;
+    while (true) {
+        let a = (Math.random() > 0.5 ? '-' : '') + Math.floor(Math.random() * 100_000_000_000).toString();
+        let b = (Math.random() > 0.5 ? '-' : '') + Math.floor(Math.random() * 100_000_000_000).toString();
+
+        let myOperation = mul(a.toString(), b.toString());
+        let jsOperation = (+a * +b).toString();
+        if (!(+myOperation === +jsOperation)) {
+            console.log('a:', a, 'b:', b);
+            console.log(myOperation, jsOperation);
+            mistakeFlag = true;
+        }
+
+        i++;
+        if (i === 10000) break;
+    }
+
+    mistakeFlag ? console.log('mistakes in mul') : console.log('mul is ok');
+}
+
+function checkDiv() {
+    let i = 0;
+    let mistakeFlag = false;
+    while (true) {
+        let a = (Math.random() > 0.5 ? '-' : '') + Math.floor(Math.random() * 100_000_000_000).toString();
+        let b = (Math.random() > 0.5 ? '-' : '') + Math.floor(Math.random() * 100_000_000_000).toString();
+
+        let myOperation = div(a.toString(), b.toString());
+        let jsOperation = (BigInt(a) / BigInt(b)).toString();
+        if (!(+myOperation === +jsOperation)) {
+            console.log('a:', a, 'b:', b);
+            console.log(myOperation, jsOperation);
+            mistakeFlag = true;
+        }
+
+        i++;
+        if (i === 10000) break;
+    }
+
+    mistakeFlag ? console.log('mistakes in div') : console.log('div is ok');
+}
+
+checkSum();
+checkSub();
+checkMul();
+checkDiv();
+
+
